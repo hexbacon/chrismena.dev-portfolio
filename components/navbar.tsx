3ddbd476ad7c753d967a26e5255ef7e5e64c5e32
Christopher Mena <you@chrismena.dev>
2024-05-04 14:51:49 -0400

    draft for navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,14 @@
+export function Navbar() {
+    return (
+        <nav className="justify-centerpy-2 flex w-full select-none pt-6 font-light md:px-28 md:pb-2">
+            <div className="container flex flex-col items-center justify-between md:flex-row">
+                <h1 className="">Chris Mena</h1>
+                <ul className="flex">
+                    <li className="px-5">About</li>
+                    <li className="px-5">About</li>
+                    <li className="px-5">About</li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
